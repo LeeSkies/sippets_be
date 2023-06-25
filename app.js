e4b6f55382c8e812d92bfc76a574751650b3811f
leeskies <lee2dskies@gmail.com>
2023-06-25 21:19:10 +0300

    yo

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const server = require('http').createServer(app)
 // middleware
 app.use(
   cors({
-    // origin: 'https://sippets.vercel.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://sippets.vercel.app',
+    // origin: 'http://localhost:5173',
     credentials: true,
     // exposedHeaders: ["Set-cookie"],
   }),
