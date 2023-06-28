1b912e618bae3e24195485084ada5490738b3475
leeskies <lee2dskies@gmail.com>
2023-06-29 00:02:56 +0300

    x

--- controllers/public/searchControllers.js
+++ controllers/public/searchControllers.js
@@ -13,7 +13,7 @@ const searchByText = async (req, res) => {
             }
           })
           .populate('author', 'username image.secure_url')
-          .select('blocks likesCount author createdAt language')
+          .select('blocks likesCount author createdAt language toastsCount commentsCount')
           .skip(offset * 20)
           .limit(20)
           res.json(results)
@@ -27,7 +27,7 @@ const searchByUser = async (req, res) => {
     const { text } = req.query
     try {
         const results = await User.find({ username: { $regex: text, $options: 'i' } })
-          .select('username image.secure_url sippetsCount followersCount followingCount createdAt')
+          .select('username image.secure_url sippetsCount followersCount followingCount createdAt bio')
           .skip(offset * 20)
           .limit(20)
           res.json(results)
