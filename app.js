8b61d2f88065ffa4fd5474500f6cd5c7384cd966
leeskies <lee2dskies@gmail.com>
2023-06-29 03:53:46 +0300

    x

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app)
 // middleware
 app.use(
   cors({
-    origin: 'https://sippets.vercel.app',
+    origin: 'https://sippets.netlify.app',
     credentials: true,
   }),
   express.json(),
